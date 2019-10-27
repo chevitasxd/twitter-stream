5493823b337453c463bb20c16b44c0612dbfa4e6
Sebastián Aguado <chevitasxd@gmail.com>
2019-10-27 23:18:33 +0100

    Fixing cors issue

--- backend-stream/Startup.cs
+++ backend-stream/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,18 +12,27 @@ namespace backend_stream
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            services.AddCors();
+            services.AddSignalR();            
             services.AddSingleton<ITweetHandler, TweetEventHandler>();
             services.AddHostedService<TweetEventWorker>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration config)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins(config["CORS_ALLOWED"])
+                    .AllowAnyHeader()
+                    .WithMethods("GET", "POST")
+                    .AllowCredentials();
+            });
 
             app.UseRouting();
             app.UseEndpoints(endpoints =>
--- web-client/server.js
+++ web-client/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
@@ -6,7 +7,7 @@ const port = 3000;
    Here, for route "/", use "static" folder.
    In "static" folder you can put now html, js, css files.
  */
-//app.get('/tweethuburl', (req, res) => res.send(process.env.TWEET_HUB_URL));
-app.get('/tweethuburl', (req, res) => res.send("sebiurl"));
+app.use(cors());
+app.get('/tweethuburl', (req, res) => res.send(process.env.TWEET_HUB_URL + '/tweethub'));
 app.use('/', express.static('public'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
